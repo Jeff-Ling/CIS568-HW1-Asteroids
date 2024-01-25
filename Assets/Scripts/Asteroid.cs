803853b6e061d34bf2df1ff22bb04e93dd298f07
Jiefu Ling <jeffling0603@gmail.com>
2024-01-25 16:59:03 -0500

    Finished

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    public GameObject deathExplosion;
+
+    public int pointValue;
+
+    public AudioClip deathKnell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointValue = 10;
+    }
     public void Die()
     {
-        // Destroy removes the gameObject from the scene and
-        // marks it for garbage collection
+        AudioSource.PlayClipAtPoint(deathKnell, gameObject.transform.position);
+        Instantiate(deathExplosion, gameObject.transform.position,
+        Quaternion.AngleAxis(-90, Vector3.right));
+        GameObject obj = GameObject.Find("GlobalObject");
+        Global g = obj.GetComponent<Global>();
+        g.score += pointValue;
         Destroy(gameObject);
     }
 }
